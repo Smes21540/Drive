24f4999a1cebcb3b8eb4d94710ca36ecbd11f8bc
Smes21540 <Kevin.goessens@smes-france.com>
2025-10-06 17:55:54 +0200

    Update drive.js

--- netlify/functions/drive.js
+++ netlify/functions/drive.js
@@ -1,1 +1,24 @@
+export async function handler(event, context) {
+  const id = event.queryStringParameters.id;
+  const key = process.env.API_KEY; // Clé stockée sur Netlify
+
+  const url = `https://www.googleapis.com/drive/v3/files/${id}?alt=media&key=${key}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    return { statusCode: response.status, body: "Erreur Google Drive" };
+  }
+
+  const data = await response.arrayBuffer();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": response.headers.get("content-type") || "application/octet-stream",
+    },
+    body: Buffer.from(data).toString("base64"),
+    isBase64Encoded: true,
+  };
+}
 
